dccd76ef02caac4334c87dcda0385533bf208138
Safal Gautam <saphal106@gmail.com>
2025-05-16 19:47:37 +0545

    Insert into week 2

---
+++ Week 2/plates/plates.py
@@ -0,0 +1,33 @@
+#Creating main function
+def main():
+
+    plate = input("Plate: ").strip() #Taking input
+
+    #Calling function to check
+    if is_valid(plate):
+        print("Valid")
+
+    else :
+        print("Invalid")
+
+#Creating function to valiadate
+def is_valid(s):
+
+    #Checking conditions
+    if not 2 <= len(s) <=6:
+        return False
+    if not s[0:2].isalpha():
+        return False
+    if not s.isalnum():
+        return False
+
+    number_found = False
+    for i in range(len(s)):
+        if s[i].isdigit():
+            if not number_found and s[i]=='0':
+                return False
+            number_found = True
+        elif number_found:
+            return False
+    return True
+main()
